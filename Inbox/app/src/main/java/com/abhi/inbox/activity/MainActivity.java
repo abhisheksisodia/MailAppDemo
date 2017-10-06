d5b52a183a77dfcfa893a16dd360e6cb5e165662
Abhishek Sisodia <abhisheksisodia13@gmail.com>
2017-10-06 11:02:10 -0400

    Added recyclerview adapter

--- Inbox/app/src/main/java/com/abhi/inbox/activity/MainActivity.java
+++ Inbox/app/src/main/java/com/abhi/inbox/activity/MainActivity.java
@@ -1,17 +1,43 @@
 package com.abhi.inbox.activity;
 
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.abhi.inbox.R;
+import com.abhi.inbox.adapter.MessagesAdapter;
+import com.abhi.inbox.helper.DividerItemDecoration;
+import com.abhi.inbox.model.Message;
+import com.abhi.inbox.network.ApiClient;
+import com.abhi.inbox.network.ApiInterface;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, MessagesAdapter.MessageAdapterListener {
+    private List<Message> messages = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MessagesAdapter mAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ActionModeCallback actionModeCallback;
+    private ActionMode actionMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +54,85 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        mAdapter = new MessagesAdapter(this, messages, this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(mAdapter);
+
+        actionModeCallback = new ActionModeCallback();
+
+        // show loader and fetch messages
+        swipeRefreshLayout.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        getInbox();
+                    }
+                }
+        );
+    }
+
+    /**
+     * Fetches mail messages by making HTTP request
+     * url: https://api.androidhive.info/json/inbox.json
+     */
+    private void getInbox() {
+        swipeRefreshLayout.setRefreshing(true);
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<List<Message>> call = apiService.getInbox();
+        call.enqueue(new Callback<List<Message>>() {
+            @Override
+            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                // clear the inbox
+                messages.clear();
+
+                // add all the messages
+                // messages.addAll(response.body());
+
+                // TODO - avoid looping
+                // the loop was performed to add colors to each message
+                for (Message message : response.body()) {
+                    // generate a random color
+                    message.setColor(getRandomMaterialColor("400"));
+                    messages.add(message);
+                }
+
+                mAdapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onFailure(Call<List<Message>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Unable to fetch json: " + t.getMessage(), Toast.LENGTH_LONG).show();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    /**
+     * chooses a random color from array.xml
+     */
+    private int getRandomMaterialColor(String typeColor) {
+        int returnColor = Color.GRAY;
+        int arrayId = getResources().getIdentifier("mdcolor_" + typeColor, "array", getPackageName());
+
+        if (arrayId != 0) {
+            TypedArray colors = getResources().obtainTypedArray(arrayId);
+            int index = (int) (Math.random() * colors.length());
+            returnColor = colors.getColor(index, Color.GRAY);
+            colors.recycle();
+        }
+        return returnColor;
     }
 
     @Override
@@ -45,10 +150,134 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            Toast.makeText(getApplicationContext(), "Search...", Toast.LENGTH_SHORT).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRefresh() {
+        // swipe refresh is performed, fetch the messages again
+        getInbox();
+    }
+
+    @Override
+    public void onIconClicked(int position) {
+        if (actionMode == null) {
+            actionMode = startSupportActionMode(actionModeCallback);
+        }
+
+        toggleSelection(position);
+    }
+
+    @Override
+    public void onIconImportantClicked(int position) {
+        // Star icon is clicked,
+        // mark the message as important
+        Message message = messages.get(position);
+        message.setImportant(!message.isImportant());
+        messages.set(position, message);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onMessageRowClicked(int position) {
+        // verify whether action mode is enabled or not
+        // if enabled, change the row state to activated
+        if (mAdapter.getSelectedItemCount() > 0) {
+            enableActionMode(position);
+        } else {
+            // read the message which removes bold from the row
+            Message message = messages.get(position);
+            message.setRead(true);
+            messages.set(position, message);
+            mAdapter.notifyDataSetChanged();
+
+            Toast.makeText(getApplicationContext(), "Read: " + message.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRowLongClicked(int position) {
+        // long press is performed, enable action mode
+        enableActionMode(position);
+    }
+
+    private void enableActionMode(int position) {
+        if (actionMode == null) {
+            actionMode = startSupportActionMode(actionModeCallback);
+        }
+        toggleSelection(position);
+    }
+
+    private void toggleSelection(int position) {
+        mAdapter.toggleSelection(position);
+        int count = mAdapter.getSelectedItemCount();
+
+        if (count == 0) {
+            actionMode.finish();
+        } else {
+            actionMode.setTitle(String.valueOf(count));
+            actionMode.invalidate();
+        }
+    }
+
+
+    private class ActionModeCallback implements ActionMode.Callback {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.menu_action_mode, menu);
+
+            // disable swipe refresh if action mode is enabled
+            swipeRefreshLayout.setEnabled(false);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_delete:
+                    // delete all the selected messages
+                    deleteMessages();
+                    mode.finish();
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mAdapter.clearSelections();
+            swipeRefreshLayout.setEnabled(true);
+            actionMode = null;
+            recyclerView.post(new Runnable() {
+                @Override
+                public void run() {
+                    mAdapter.resetAnimationIndex();
+                    // mAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+    // deleting the messages from recycler view
+    private void deleteMessages() {
+        mAdapter.resetAnimationIndex();
+        List<Integer> selectedItemPositions =
+                mAdapter.getSelectedItems();
+        for (int i = selectedItemPositions.size() - 1; i >= 0; i--) {
+            mAdapter.removeData(selectedItemPositions.get(i));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
